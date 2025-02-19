edee3d4e2104488eb7f74f6bdacba552d963ae78
unknown <caldererortiz2005@gmail.com>
2025-02-19 10:55:36 +0100

    pr

--- Tema4/eJ_o6.sql
+++ Tema4/eJ_o6.sql
@@ -67,26 +67,117 @@ INSERT INTO proyecciones VALUES ('S6','P10','23.00',68);
 INSERT INTO proyecciones VALUES ('S6','P9','23.00',50);
 INSERT INTO proyecciones VALUES ('S4','P8','12.00',36);
 
+//CONSULTAS
+
+--1
+SELECT nombre FROM peliculas;
+
+--2
+SELECT DISTINCT calificacion FROM peliculas;
+
+--3
+SELECT nombre FROM peliculas WHERE calificacion IS NULL;
+
+--4
+SELECT * FROM salas ORDER BY capacidad DESC;
+
+--5
+SELECT salas 
+
+--6
+
 -- 7
 
 -- 8
 
 -- 9
 
+-- 10
+
+-- 11
+
+-- 12
+
+-- 13
+
 -- 14
+
+-- 15
 
 -- 16
 
+-- 17
+
+-- 18
+
+-- 19
+
 -- 20
+
+-- 21
 
 -- 22
 
+-- 23
+
+-- 24
+
+-- 25
+
+-- 26
+
+-- 27
+
+-- 28
+
+-- 29
+
+-- 30
+
 -- 31
+
+-- 32
+
+-- 33
+
+-- 34
 
 -- 35
 
 -- 36
 
+-- 37
+
+-- 38
+
 -- 39
 
+-- 40
+
+-- 41
+
+-- 42
+
 -- 43
+
+-- 44
+
+-- 45
+
+-- 46
+
+-- 47
+
+-- 48
+
+-- 49
+
+-- 50
+
+-- 51
+
+-- 52
+
+-- 53
+
+-- 54
